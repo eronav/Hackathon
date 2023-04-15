5e1373c8028ef4c4ea8775aa6ede78ab71552e63
Ronav <mailronav@gmail.com>
2023-04-15 14:24:22 -0700

    first commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Ronav Dholakia on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
